8fbf0f124a66de1fa6a160ce60b503344462d972
Taylor C. Richberger <taywee@gmx.com>
2020-03-26 12:04:30 -0600

    display palette

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod color;
 pub mod design;
 use color::NHPaletteItemPair;
 use design::Design;
-use std::collections::HashMap;
 
 use wasm_bindgen::prelude::*;
 
@@ -13,7 +12,7 @@ pub fn main() -> Result<(), JsValue> {
     // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+    document.body().expect("document should have a body");
 
     Ok(())
 }
