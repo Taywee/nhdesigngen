5aca0d19081f4e993f7b8bcbd95c6cabb1c643ac
Taylor C. Richberger <taywee@gmx.com>
2020-03-26 09:02:45 -0600

    start switching to webassembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,31 @@
 pub mod color;
 pub mod design;
-pub mod gtk;
+use design::Design;
 
-use image::io::Reader;
-use std::io::{BufRead, Seek};
+use wasm_bindgen::prelude::*;
 
-pub struct Config<R>
-where
-    R: BufRead + Seek,
-{
-    pub input: Reader<R>,
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn design_new() -> *mut Design {
+    Box::into_raw(Box::new(Design::default()))
+}
+
+#[wasm_bindgen]
+pub fn design_print_palette(design: *const Design) -> String {
+    if let Some(design) = unsafe { design.as_ref() } {
+        format!("design palette: {:?}", design.palette())
+    } else {
+        panic!("AAAAAAHHHH");
+    }
 }
